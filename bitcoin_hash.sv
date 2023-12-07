818bf076fec1c4fb25750ebfdb891c3539aed35b
Jesse Vernallis <92563135+jesseVernallis@users.noreply.github.com>
2023-12-06 23:26:35 -0800

    Works for 4 instance parallelism
    803 clocks

--- bitcoin_hash.sv
+++ bitcoin_hash.sv
@@ -6,12 +6,13 @@ module bitcoin_hash (input logic        clk, reset_n, start,
                      input logic [31:0] memory_read_data);
 
 parameter num_nonces = 16;
+parameter INSTANCES = 4;
 logic [31:0] hash_out[num_nonces];
 
 
    // FSM state variables 
    //                 0     1       2        3        4     5          
-   enum logic [2:0] {IDLE, READ, PAD_BLOCK, COMPUTE, ADD, WRITE} next_state;
+   enum logic [2:0] {IDLE, READ, COMPUTE_ST0, ADD_ST0, PAD_BLOCK, COMPUTE, ADD,WRITE} next_state;
    
    
    // NOTE : Below mentioned frame work is for reference purpose.
@@ -36,8 +37,31 @@ logic [31:0] hash_out[num_nonces];
 	logic [4:0] nonce;
    logic [4:0] word_offset;
 	logic [31:0] saved_message[3];
-	
 	logic [31:0] message_out [16];
+	
+	logic [31:0] w_p[INSTANCES][64]; // hash computation temporary variable
+	logic [31:0] messages_p[INSTANCES][16]; //temporary BLOCK storage (16 WORDS)
+
+	logic [31:0] hashnum_p[INSTANCES][8]; //INITIAL hash and MIDDLE hash values
+	logic [31:0] A_[INSTANCES];
+	logic [31:0] B_[INSTANCES];
+	logic [31:0] C_[INSTANCES];
+	logic [31:0] D_[INSTANCES];
+	logic [31:0] E_[INSTANCES];
+	logic [31:0] F_[INSTANCES];
+	logic [31:0] G_[INSTANCES];
+	logic [31:0] H_[INSTANCES];
+	
+	logic [31:0] hash0_[INSTANCES];
+	logic [31:0] hash1_[INSTANCES];
+	logic [31:0] hash2_[INSTANCES];
+	logic [31:0] hash3_[INSTANCES];
+	logic [31:0] hash4_[INSTANCES];
+	logic [31:0] hash5_[INSTANCES];
+	logic [31:0] hash6_[INSTANCES];
+	logic [31:0] hash7_[INSTANCES];	//OUTPUT hash and MIDDLE hash values
+	logic [31:0] common_hash_p[8];
+	logic[31:0]  message_out_p[INSTANCES][16];
 
 	
 	parameter longint SIZE = 640; 
@@ -124,15 +148,15 @@ logic [31:0] hash_out[num_nonces];
             output_message[15] = SIZE[31:0];
      endfunction
 	 // pads the message for the 3rd stage
-	  function void pad_message_h(input logic [31:0] hash0_h,hash1_h,hash2_h,hash3_h,hash4_h,hash5_h,hash6_h,hash7_h, output logic [31:0] output_message[16]);
-        output_message[0]=hash0_h;
-		  output_message[1]=hash1_h;
-		  output_message[2]=hash2_h;
-		  output_message[3]=hash3_h;
-		  output_message[4]=hash4_h;
-		  output_message[5]=hash5_h;
-		  output_message[6]=hash6_h;
-		  output_message[7]=hash7_h;
+	  function void pad_message_h(input logic [31:0] hash0,hash1,hash2,hash3,hash4,hash5,hash6,hash7, output logic [31:0] output_message[16]);
+        output_message[0]=hash0;
+		  output_message[1]=hash1;
+		  output_message[2]=hash2;
+		  output_message[3]=hash3;
+		  output_message[4]=hash4;
+		  output_message[5]=hash5;
+		  output_message[6]=hash6;
+		  output_message[7]=hash7;
 		  
         output_message[8] = 32'h80000000;
 		  
@@ -214,7 +238,7 @@ logic [31:0] hash_out[num_nonces];
 					if(stage == 0) begin 
 						//compute_w setup
 						round_index <= 0;
-						next_state <=  COMPUTE;
+						next_state <=  COMPUTE_ST0;
 					end
 					else begin
 						//pad_block setup
@@ -227,7 +251,7 @@ logic [31:0] hash_out[num_nonces];
 		   end
 			
 			//ADD OPERATION
-		   ADD: begin 
+		   ADD_ST0: begin 
 			   //ADD OPERATION
 			   hash0 <= hash0+A;
 				hash1 <= hash1+B;
@@ -269,57 +293,11 @@ logic [31:0] hash_out[num_nonces];
 						common_hash[7] <= hash7+H;
 						next_state <= READ;
 					end
-					else if(stage == 1)begin
-						next_state <= PAD_BLOCK;
-					end
-					else if(stage == 2)	begin
-						present_addr <= hash_out_addr + nonce;
-						present_write_data <= hash0+A;
-						enable_write_reg <= 1;
-						word_offset <= 0;
-						next_state <= WRITE;
-					end
 			   //end
 		   end
-			
-			//Pad the message if needed
-			PAD_BLOCK: begin
-				//message set to message out reg from module
-
-				if(stage == 1) begin
-					pad_message_nonce(saved_message,nonce,message_out);
-					A <= common_hash[0];
-					B <= common_hash[1];
-					C <= common_hash[2];
-					D <= common_hash[3];
-					E <= common_hash[4];
-					F <= common_hash[5];
-					G <= common_hash[6];
-					H <= common_hash[7];
-					hash0 <= common_hash[0];
-					hash1 <= common_hash[1];
-					hash2 <= common_hash[2];
-					hash3 <= common_hash[3];
-					hash4 <= common_hash[4];
-					hash5 <= common_hash[5];
-					hash6 <= common_hash[6];
-					hash7 <= common_hash[7];
-				end
-				else if(stage == 2) begin
-					pad_message_h(hash0,hash1,hash2,hash3,hash4,hash5,hash6,hash7,message_out);
-				{A, B, C, D, E, F, G, H} <= {32'h6a09e667, 32'hbb67ae85, 32'h3c6ef372,
-				32'ha54ff53a, 32'h510e527f, 32'h9b05688c, 32'h1f83d9ab, 32'h5be0cd19};
-				{hash0 , hash1, hash2, hash3, hash4, hash5, hash6, hash7} <= {32'h6a09e667, 32'hbb67ae85, 32'h3c6ef372,
-				32'ha54ff53a, 32'h510e527f, 32'h9b05688c, 32'h1f83d9ab, 32'h5be0cd19};	
-				end
-				message <= message_out;
-				//setup for compute_w
-				round_index <= 0;	
-				next_state <=  COMPUTE;
-			end
 			// 64 round of COMPRESSION ALGORITHM 
 			/*********DONE***********/
-		   COMPUTE: begin 
+		   COMPUTE_ST0: begin 
 		   	   // if there are still rounds left
 			   if(round_index<=64) begin 
 					//Compute W[0] -> W[63] from input padded message
@@ -337,23 +315,153 @@ logic [31:0] hash_out[num_nonces];
 					else begin
 						round_index <= round_index + 1;
 					end
-				next_state <= COMPUTE;
+				next_state <= COMPUTE_ST0;
 			   end
 			   //final round
 			   else begin
 					//block setup
-					next_state <= ADD;
+					next_state <= ADD_ST0;
 			   end
 		   end
+			//Pad the message if needed
+			PAD_BLOCK: begin
+				//message set to message out reg from module
+				if(stage == 1) begin
+					for(int inst=0;inst<INSTANCES;inst++) begin
+						pad_message_nonce(saved_message,nonce + inst,message_out_p[inst]);
+						A_[inst] <= hash0;
+						B_[inst] <= hash1;
+						C_[inst] <= hash2;
+						D_[inst] <= hash3;
+						E_[inst] <= hash4;
+						F_[inst] <= hash5;
+						G_[inst] <= hash6;
+						H_[inst] <= hash7;
+						hash0_[inst] <= hash0;
+						hash1_[inst] <= hash1;
+						hash2_[inst] <= hash2;
+						hash3_[inst] <= hash3;
+						hash4_[inst] <= hash4;
+						hash5_[inst] <= hash5;
+						hash6_[inst] <= hash6;
+						hash7_[inst] <= hash7;
+					end
+				end
+				else if(stage == 2) begin
+					for(int inst=0;inst<INSTANCES;inst++) begin
+						pad_message_h(hash0_[inst],hash1_[inst],hash2_[inst],hash3_[inst],hash4_[inst],hash5_[inst],hash6_[inst],hash7_[inst],message_out_p[inst]);
+					{A_[inst], B_[inst], C_[inst], D_[inst], E_[inst], F_[inst], G_[inst], H_[inst]} <= {32'h6a09e667, 32'hbb67ae85, 32'h3c6ef372,
+					32'ha54ff53a, 32'h510e527f, 32'h9b05688c, 32'h1f83d9ab, 32'h5be0cd19};
+					{hash0_[inst], hash1_[inst], hash2_[inst], hash3_[inst], hash4_[inst], hash5_[inst], hash6_[inst], hash7_[inst]} <= {32'h6a09e667, 32'hbb67ae85, 32'h3c6ef372,
+					32'ha54ff53a, 32'h510e527f, 32'h9b05688c, 32'h1f83d9ab, 32'h5be0cd19};	
+					end
+				end
+				for(int inst=0;inst<INSTANCES;inst++) begin
+					messages_p[inst] <= message_out_p[inst];
+				end
+				//setup for compute_w
+				round_index <= 0;	
+				next_state <=  COMPUTE;
+			end
+				//ADD OPERATION
+		   ADD: begin 
+				for(int inst=0;inst<INSTANCES;inst++) begin
+					//ADD OPERATION
+					hash0_[inst] <= hash0_[inst]+A_[inst];
+					hash1_[inst] <= hash1_[inst]+B_[inst];
+					hash2_[inst] <= hash2_[inst]+C_[inst];
+					hash3_[inst] <= hash3_[inst]+D_[inst];
+					hash4_[inst] <= hash4_[inst]+E_[inst];
+					hash5_[inst] <= hash5_[inst]+F_[inst];
+					hash6_[inst] <= hash6_[inst]+G_[inst];
+					hash7_[inst] <= hash7_[inst]+H_[inst];
+					
+					// if this is the last block, WRITE
+					//if(nonce == 16)begin 
+					//for parallel change
+						
+						//next_state <= WRITE;
+					//end
+					//if it is not the last block, begin READ
+					//else begin 
+						//TODO .
+					A_[inst] <= hash0_[inst]+A_[inst];
+					B_[inst] <= hash1_[inst]+B_[inst];
+					C_[inst] <= hash2_[inst]+C_[inst];
+					D_[inst] <= hash3_[inst]+D_[inst];
+					E_[inst] <= hash4_[inst]+E_[inst];
+					F_[inst] <= hash5_[inst]+F_[inst];
+					G_[inst] <= hash6_[inst]+G_[inst];
+					H_[inst] <= hash7_[inst]+H_[inst];
+				end	
+				stage <= stage + 1;
+				if(stage == 1)begin
+					next_state <= PAD_BLOCK;
+				end
+				else if(stage == 2)	begin
+					present_addr <= hash_out_addr + nonce;
+					for(int inst=0;inst<INSTANCES;inst++) begin
+						hash_out[inst+nonce]<= hash0_[inst]+A_[inst];
+					end
+					present_write_data <= hash0_[0]+A_[0];
+					enable_write_reg <= 1;
+					word_offset <= 1;
+					next_state <= WRITE;
+				end
+			end
+			
+			
+			// 64 round of COMPRESSION ALGORITHM 
+			/*********DONE***********/
+		   COMPUTE: begin 
+	
+		   	   // if there are still rounds left
+			   if(round_index<=64) begin 
+					//Compute W[0] -> W[63] from input padded message
+					for(int inst = 0;inst<INSTANCES;inst++) begin
+						if(round_index<=15) begin
+							w_p[inst][round_index] <= messages_p[inst][round_index];
+						end else begin
+							w_p[inst][round_index] <= expand_message(w_p[inst], round_index); 
+						end
+						
+						if(round_index != 0) begin
+						// 64 round of COMPRESSION ALGORITHM
+							{A_[inst], B_[inst], C_[inst], D_[inst], E_[inst], F_[inst], G_[inst], H_[inst]} <= sha256_op(A_[inst], 
+							B_[inst], C_[inst], D_[inst], E_[inst], F_[inst], G_[inst], H_[inst], w_p[inst][round_index-1], round_index-1);
+							round_index <= round_index + 1;
+						end
+						else begin
+							round_index <= round_index + 1;
+						end
+					end		
+					next_state <= COMPUTE;
+				end
+				//final round
+				else begin
+					//block setup
+					next_state <= ADD;
+				end
+		   end
+			
+			
 			//Write has values back to memory
 		   WRITE: begin
-				nonce <= nonce + 1;
-				if(nonce == 15) begin
-					next_state <= IDLE;
+				if(word_offset < INSTANCES && nonce < 15) begin
+					present_write_data <= hash_out[word_offset+nonce];
+					present_addr <= hash_out_addr + nonce + word_offset;
+					word_offset <= word_offset + 1;
+					next_state <= WRITE;
 				end
 				else begin
-					stage <= 1;
-					next_state <= PAD_BLOCK;
+					if(nonce >= 15) begin
+						next_state <= IDLE;
+					end
+					else begin
+						stage <= 1;
+						nonce <= nonce + INSTANCES;
+						next_state <= PAD_BLOCK;
+					end
 				end
 			end
 		endcase
